9b5dff99ce57a2021fd0a25b7b79b7994e8947b0
Abdifatahz <faahid44@gmail.com>
2023-02-24 11:05:06 -0700

    06_02_Migrating Categories and Lessons Table.

---
+++ database/migrations/2023_02_24_175934_create_lessons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("slug")->unique();
+            $table->text('description');
+            $table->foreignId("parent_id")->nullable();
+            $table->boolean("is_visible");
+            $table->boolean("is_free");
+            $table->integer("position")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
