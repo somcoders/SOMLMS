86e6cd600d37fabad2a07910ba7d6250747c2477
Abdifatahz <faahid44@gmail.com>
2023-02-24 11:04:51 -0700

    06_01 Database Migration 101

---
+++ database/migrations/2023_02_24_172915_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description');
+            $table->foreignId("instructor_id");
+            $table->foreignId("category_id");
+            $table->decimal("price");
+            $table->enum("level", ["beginer","intermidiate","advanced"]);
+            $table->string("gradient");
+            $table->integer("duration");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
