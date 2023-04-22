7234487e621090a78d58a782d196c9a4e6b26aa0
Abdifatahz <faahid44@gmail.com>
2023-04-22 13:36:36 -0600

    07_02 ORM relationships.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
     use HasFactory;
 
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
     public function scopeVisible($query)
     {
--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Lesson;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Lesson extends Model
 {
     use HasFactory;
+
+    public function lessons()
+    {
+        return $this->hasMany(Lesson::class, 'parent_id');
+    }
+
+      public function chapter()
+      {
+          return $this->belongsTo(Lesson::class, 'parent_id');
+      }
 }
