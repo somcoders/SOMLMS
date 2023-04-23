3408e6e661a4963ada3cab5ac819d0f1b1d7419c
Abdifatahz <faahid44@gmail.com>
2023-04-23 12:52:57 -0600

    08_03 Added Breeze Auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CourseController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 
 /*
@@ -11,16 +12,17 @@ use App\Http\Controllers\CategoryController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-//Using Tinker
+
+
 Route::get('/', HomeController::class)->name("home");
 
 
-Route::get('/profile', function () {
+Route::get('/student/profile', function () {
     return view('profile');
 })->name("profile");
 
@@ -29,3 +31,16 @@ Route::get("/courses", [CourseController::class,"index"])->name("courses.index")
 Route::get("/course/{slug}/{lesson?}", [CourseController::class,"show"])->name("courses.show");
 
 Route::resource("/categories", CategoryController::class)->only(["index","show"]);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [],
+
+    plugins: [require("@tailwindcss/forms")],
     safelist: [
         {
             pattern: /from-(purple|fuchsia|blue)-(400|500)/,
